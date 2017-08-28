650333db2680ad649ad6a8eeecd20ef34973b371
AllisonRKahn <31288751+AllisonRKahn@users.noreply.github.com>
2017-08-28 15:41:03 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+var http = require('http'),
+    fs = require('fs'),
+    url = require('url'),
+    port = 8080;
+
+/* Global variables */
+var listingData, server;
+
+var requestHandler = function(request, response) {
+  var parsedUrl = url.parse(request.url);
+
+    if(parsedUrl.path == '/listings')
+    {
+      response.writeHead(200);
+      response.end(listingData);
+    }
+  else {
+    response.writeHead(404);
+    response.end('Bad gateway error');
+  }
+}
+
+
+
+fs.readFile('listings.json', (err, data) => {
+  if (err) throw err;
+  data = JSON.parse(data);
+  data = JSON.stringify(data);
+  listingData = data;
+
+});
+
+var server = http.createServer(requestHandler);
+server.listen(port, function() {
+});
